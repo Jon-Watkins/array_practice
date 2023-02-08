e7f0c539b52395462a611a07f9faae6858d6cba5
Jon Watkins <jkwatkins85@gmail.com>
2023-02-08 12:32:32 -0700

    Add index position examples

--- arrays.rb
+++ arrays.rb
@@ -3,25 +3,35 @@ ages = [37, 31, 37, 27]
 change = [0.25, 1.25, 0.75, 0.5]
 local = [true, true, false, true]
 
+puts friends[3]
+puts ages[2]
+puts change[3]
+puts local[3]
+
 =begin
 Calling friends.pop will remove (or 'pop') the final element 
 in the array and return the removed element.
 =end
-friends.pop
+puts friends.pop
 =begin
 Calling ages.shift will remove the first element in the array 
 and return the removed element or nil if the array is empty.
 =end
-ages.shift
+puts ages.shift
 =begin
 Calling change.unshift will add the argumented element as the 
 first element in the array and return the full array with the 
 newly added element.
 =end
-change.unshift(0.5)
+puts change.unshift(0.5)
 =begin
 Calling local.push will add the argumented element as the 
 last element in the array and return the full array with the 
 newly added element.
 =end
-local.push(false)
+puts local.push(false)
+
+puts friends[3]
+puts ages[2]
+puts change[3]
+puts local[3]
