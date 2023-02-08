91bb8043a7ec74d8a43318125995f27741152687
Jon Watkins <jkwatkins85@gmail.com>
2023-02-08 13:01:17 -0700

    Add count method

--- arrays.rb
+++ arrays.rb
@@ -8,6 +8,8 @@ puts ages[2]
 puts change[3]
 puts local[3]
 
+puts friends.count("Cat")
+
 =begin
 Calling friends.pop will remove (or 'pop') the final element 
 in the array and return the removed element.
@@ -35,3 +37,38 @@ puts friends[3]
 puts ages[2]
 puts change[3]
 puts local[3]
+
+=begin
+The count method returns the number of elements within the called array; 
+if no argument is given, it will count how many elements there are 
+in total, while adding an argument will return the number of the
+specific argumented elements in the array. The method uses the '=='
+conditional operator.
+Should a block (a small function which can be included in a method) 
+be given, the method will only provide the number of elements within 
+the array which the block returns a true value for.
+=end
+friends = ["Cat", "Grant", "Sarah", "Zoe"]
+ages = [37, 31, 37, 27]
+puts "Let's first see how many friends there are in the friends array."
+puts friends.count
+puts "Let's check how many times Cat shows up."
+puts friends.count("Cat")
+puts "Now we'll remove the first element in the friends array."
+puts friends.shift
+puts "Now lets count how many friends there are and how many times Cat appears in the friends array."
+puts friends.count
+puts friends.count("Cat")
+puts "Now let's add Cat back to the array..."
+puts friends.push("Cat")
+puts "... and add Cat to the array in the first position as well."
+puts friends.unshift("Cat")
+puts "And now let's see how many times Cat is present."
+puts friends.count("Cat")
+puts "Now let's see how many of our friends are 37..."
+puts ages.count(37)
+puts "... and how many are over 30."
+puts ages.count{|x| x > 30}
+puts "Now let's find out how many of our friends will be 40 by 2025."
+puts ages.count{|x| x + 3 == 40}
+
